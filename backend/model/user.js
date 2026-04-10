315d88c5b7c73a0a0250ca95017bfb91a2b3fd31
Guillaume <guillaumegface@gmail.com>
2026-04-10 09:38:50 -0400

    CODAGE: Schema mongoose pour user.

---
+++ backend/model/user.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  user: { type: String, required: true },
+  password: { type: String, required: true },
+  fullName: { type: String, required: true },
+  email: { type: String, required: true },
+  //access( Ce qui donne access a tout les modules. Rajout de certain premade modules pour future proofing)
+  access: {
+    type: Map,
+    of: mongoose.Schema.Types.Mixed,
+    defaults: {
+      music: null,
+      video: null,
+      homeAssist: null,
+      tokenGen: null,
+      storage: null,
+      AI: null,
+    },
+  },
+});
