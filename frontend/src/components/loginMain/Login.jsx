3e03bbc7ddd4d7a5165695960b41b1a29c148ade
Guillaume <guillaumegface@gmail.com>
2026-04-24 09:33:43 -0400

    CODAGE frontend: page de login half.

---
+++ frontend/src/components/loginMain/Login.jsx
@@ -0,0 +1,71 @@
+import { useState, useContext } from "react";
+import "./Login.css";
+import { Link, useNavigate } from "react-router-dom";
+import { AuthContext } from "../../context/auth-context";
+import ModalMessageErreur from "../UIElements/ModalMessageErreur";
+import Squared from "../UIElements/loadingSquare";
+import { useHttpClient } from "../../hooks/http-hook";
+
+export default function Login() {
+  const navigate = useNavigate();
+  const auth = useContext(AuthContext);
+
+  const { isLoading, error, sendRequest, clearError } = useHttpClient();
+  const [entredValues, setEntredValues] = useState({
+    username: "",
+    password: "",
+  });
+
+  //Va chercher le last input? renter a ^
+  const handleInputChange = (identifier, value) => {
+    setEntredValues((prevValue) => ({
+      ...prevValue,
+      [identifier]: value,
+    }));
+  };
+
+  const authSubmitHandler = async (event) => {
+    event.preventDefault();
+    try {
+      const response = await sendRequest(
+        "http://localhost:5000/api/users/login",
+        "POST",
+        JSON.stringify(entredValues),
+        {
+          "Content-Type": "application/json",
+        },
+      );
+      auth.login(response.userId, response.token);
+      navigate("/hub");
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  return (
+    <>
+      <div>
+        {isLoading && <Squared />}
+        <ModalMessageErreur message={error} onClose={() => clearError()} />
+      </div>
+      <form onSubmit={authSubmitHandler}>
+        <h2>Login</h2>
+
+        <div className="input_form">
+          <div className="input_lane">
+            <label htmlFor="username">Username</label>
+            <input
+              id="username"
+              type="username"
+              name="username"
+              onChange={(event) =>
+                handleInputChange("username", event.target.value)
+              }
+              value={entredValues.username}
+            />
+          </div>
+        </div>
+      </form>
+    </>
+  );
+}
