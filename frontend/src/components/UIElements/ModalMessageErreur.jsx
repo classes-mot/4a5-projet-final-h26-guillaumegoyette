ae14f80280a32f58f3c83be94a95c83d28ae026c
GuillaumeGoy <guillaumegface@gmail.com>
2026-05-05 22:04:21 -0400

    CODAGE/DEBUG frontend-backend

--- frontend/src/components/UIElements/ModalMessageErreur.jsx
+++ frontend/src/components/UIElements/ModalMessageErreur.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const ModalMessageErreur = ({ message, Onclose }) => {
+const ModalMessageErreur = ({ message, onClose }) => {
   if (!message) return null;
 
   return (
